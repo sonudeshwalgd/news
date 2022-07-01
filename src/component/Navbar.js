2b79e9eb6d934d164a4d50a00aefd6b67df29a13
sonudeshwalgd <101305714+sonudeshwalgd@users.noreply.github.com>
2022-07-01 21:01:41 +0530

    let see the git ignore

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,18 +4,8 @@ return(
 <div>here we just changes the navbar at github</div>
 <div>her ethe chge we done from vsc</div>
 <div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
-<div>her ethe chge we done from vsc</div>
+<div>her ethe from vsc</div>
+<div>hehe chge we done from vsc</div>
 <div>her ethe chge we done from vsc</div>
 <div>her ethe chge we done from vsc</div>
 </>)
